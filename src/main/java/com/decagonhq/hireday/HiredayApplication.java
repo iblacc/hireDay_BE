a73e8418c4926ad3f0d673a06ad5d0bcc516e58d
Shuaib Bashir <bashonbash@gmail.com>
2020-05-10 23:41:29 +0100

    Initial app setup

---
+++ src/main/java/com/decagonhq/hireday/HiredayApplication.java
@@ -0,0 +1,13 @@
+package com.decagonhq.hireday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiredayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HiredayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/decagonhq/hireday/HiredayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.decagonhq.hireday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiredayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
