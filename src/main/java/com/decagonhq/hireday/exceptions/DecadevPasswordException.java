33efa07f39ceb2fc845e45494834fe64fa47b2a2
Shuaib Bashir <bashonbash@gmail.com>
2020-05-14 13:45:25 +0100

    add password exception

--- src/main/java/com/decagonhq/hireday/exceptions/DecadevPasswordException.java
+++ src/main/java/com/decagonhq/hireday/exceptions/DecadevPasswordException.java
@@ -1,4 +1,12 @@
 package com.decagonhq.hireday.exceptions;
 
-public class DecadevPasswordException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class DecadevPasswordException extends RuntimeException {
+
+    public DecadevPasswordException(String message) {
+        super(message);
+    }
 }
--- src/main/java/com/decagonhq/hireday/exceptions/DecadevPasswordExceptionResponse.java
+++ src/main/java/com/decagonhq/hireday/exceptions/DecadevPasswordExceptionResponse.java
@@ -1,4 +1,18 @@
 package com.decagonhq.hireday.exceptions;
 
 public class DecadevPasswordExceptionResponse {
+
+    private String password;
+
+    public DecadevPasswordExceptionResponse(String password) {
+        this.password = password;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
